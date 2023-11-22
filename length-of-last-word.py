7f2c488defe3e6851c61a481d593e4b31f5afd83
DavidHotaran <hotaran.d98@gmail.com>
2023-11-22 10:25:50 -0700

    new problem

--- length-of-last-word.py
+++ length-of-last-word.py
@@ -8,3 +8,22 @@ class Solution:
                 return ret
             ret += 1
         return ret
+
+
+# Solution 2
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        ret = 0
+        i = len(s) - 1
+
+        for j in range(i, -1, -1):
+            if s[j] != " ":
+                break
+            i -= 1
+
+        for j in range(i, -1, -1):
+            if s[j] == " ":
+                break
+            ret += 1
+
+        return ret
